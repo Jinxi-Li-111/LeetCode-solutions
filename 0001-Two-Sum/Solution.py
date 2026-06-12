5acbf560e72078327f6ecf89355e0234c9469f30
Jinxi-Li-111 <53359591@qq.com>
2026-06-12 19:44:03 -0400

    Solve LeetCode 0001

--- 0001-Two-Sum/Solution.py
+++ 0001-Two-Sum/Solution.py
@@ -3,11 +3,6 @@ from typing import List
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """
-        Given an array of integers nums and an integer target,
-        return indices of the two numbers such that they add up
-        to target
-        """
         dct = {}
         for i in range(len(nums)):
             if target - nums[i] in dct:
